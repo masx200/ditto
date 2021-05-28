618b6fd63e422d8cc73db2db077b845b8e458d0b
masx200 <masx200@qq.com>
2021-05-28 10:47:39 +0800

    Update mymounted.js

--- src/mymounted.js
+++ src/mymounted.js
@@ -1,8 +1,8 @@
 import $ from "jquery";
 import { initloadingid } from "./mark-down-reader.js";
 import { myonhashchangehandler } from "./onhashchange.js";
-import { 内容调整左边偏移, 挂载初始 } from "./render.js";
-
+import { 内容调整左边偏移 } from "./render.js";
+import { 挂载初始 } from "./挂载初始";
 export async function mymounted() {
     myonhashchangehandler();
     $("#my主体").css("padding-top", $("#my导航栏").height());
