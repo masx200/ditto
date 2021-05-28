ccf34821cfb9d458595e3050d4e90839c05bf23a
masx200 <masx200@qq.com>
2021-05-28 15:26:56 +0800

    Update toprevpage.js

--- src/toprevpage.js
+++ src/toprevpage.js
@@ -1,17 +1,25 @@
 import { ApphomeVm } from "./mark-down-reader.js";
 import { menulist } from "./menulist.js";
-
-export function toprevpage() {
+function findindexexmenulist() {
     console.log(menulist);
     var currentpageurl = ApphomeVm.urltext;
     console.log(currentpageurl);
     var currentpageindex = menulist.findIndex((a) => a === currentpageurl);
     console.log(currentpageindex);
+    return currentpageindex;
+}
+export function toprevpage() {
+    var currentpageindex = findindexexmenulist();
+
+    if (!(currentpageindex >= 0 && currentpageindex <= menulist.length - 1)) {
+        console.error("没有在菜单列表中找到当前页面");
+        return;
+    }
 }
 export function tonextpage() {
-    console.log(menulist);
-    var currentpageurl = ApphomeVm.urltext;
-    console.log(currentpageurl);
-    var currentpageindex = menulist.findIndex((a) => a === currentpageurl);
-    console.log(currentpageindex);
+    var currentpageindex = findindexexmenulist();
+    if (!(currentpageindex >= 0 && currentpageindex <= menulist.length - 1)) {
+        console.error("没有在菜单列表中找到当前页面");
+        return;
+    }
 }
