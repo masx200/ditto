b42f46bcef31c9e6f38369f2738887a120ed0ee8
masx200 <masx200@qq.com>
2021-05-28 14:10:16 +0800

    Update onhashchange.js

--- src/onhashchange.js
+++ src/onhashchange.js
@@ -3,7 +3,7 @@ import $ from "jquery";
 import { ApphomeVm } from "./mark-down-reader";
 
 export function Adjustthedistance() {
-    scrollTo(0, 0);
+    // scrollTo(0, 0);
     $("#mybody-143af32b9b8f396b798aeb8d4ee68ed9ca3").css(
         "padding-top",
         $("#mynavigationbar-dceff036a563faf668b4d4a50fd702d1d95").height()
