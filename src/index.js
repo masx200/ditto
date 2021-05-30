596ac1d9eabd9204902247b1a71c97ae82bf50de
masx200 <34191203+masx200@users.noreply.github.com>
2021-05-29 23:31:38 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { config, mount } from "./export.js";
 Object.assign(config, {
-    doctitle: "Markdown Reader",
+    mainitle: "Markdown Reader",
     subtitle: "Lightweight Markdown Documentation System",
-    index: "README.md",
-    summary: "summary.md",
+    homepage: "README.md",
+    catalogue: "summary.md",
     baseurl: "https://masx200.github.io/markdown-reader/",
 });
 var rootele = document.getElementById("root");
