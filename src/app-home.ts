19660b4552e07c46351b919c975e36de8873250f
masx200 <masx200@qq.com>
2021-05-31 22:08:13 +0800

    添加ref

--- src/app-home.ts
+++ src/app-home.ts
@@ -3,10 +3,13 @@ import { 内容调整左边偏移 } from "./render.js";
 import { tonextpage, toprevpage } from "./toprevpage.js";
 //import { mymounted } from "./mymounted.js";
 import { defineComponent } from "@vue/composition-api";
+
+import {Directorycontent_4dd4b8ef845d1a0de9b7e29b0e9bd1be517 ,markdowncontent_2e4c728cac441a0c48939881c2c714361a0 ,Mysidebar_c41e47b3b3bbc85fdbb7dbba7d3a0743644 }from "./refele.js"
 export default defineComponent({
     setup(props, { attrs, slots, emit }) {
         console.log({ props, attrs, slots, emit });
-        return {};
+        return {
+Directorycontent_4dd4b8ef845d1a0de9b7e29b0e9bd1be517 ,markdowncontent_2e4c728cac441a0c48939881c2c714361a0 ,Mysidebar_c41e47b3b3bbc85fdbb7dbba7d3a0743644};
     },
     mounted() {
         (async () => {
@@ -53,16 +56,4 @@ export default defineComponent({
     methods: {
         togglecebian() {
             // @ts-ignore
-            this.xianshicebianlan = !this.xianshicebianlan;
-            内容调整左边偏移();
-        },
-        scrolltotop() {
-            scrollTo(0, 0);
-        },
-        scrolltobottom() {
-            scrollTo(0, document.body.scrollHeight);
-        },
-        toprevpage: toprevpage,
-        tonextpage: tonextpage,
-    },
-});
+     
