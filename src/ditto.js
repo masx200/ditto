fccb3be665e4637d81621bc957becd182aeaa72d
masx200 <masx200@qq.com>
2021-05-31 14:32:46 +0800

    删除无用的动画

--- src/ditto.js
+++ src/ditto.js
@@ -3,7 +3,6 @@ import config from "./config.js";
 import { initialize } from "./initialize.js";
 export const cachemarkdown = new Map();
 ("use strict");
-var loading_interval;
 var loading_id = $("#loading-36bec0d286ad9ab9ca8c1d923b0095c1c06");
 var ditto = {
     run: initialize,
@@ -11,12 +10,8 @@ var ditto = {
 };
 export function show_loading() {
     loading_id.show();
-    loading_interval = setInterval(function () {
-        loading_id.fadeIn(1000).fadeOut(1000);
-    }, 2000);
 }
 export function stop_loading() {
-    clearInterval(loading_interval);
     loading_id.hide();
     $("#loadingparent-b80f303d89182a8e58b02981c0822deb975").hide();
 }
--- src/ditto.ts
+++ src/ditto.ts
@@ -6,7 +6,7 @@ import { initialize } from "./initialize.js";
 export const cachemarkdown = new Map();
 ("use strict");
 
-var loading_interval: ReturnType<typeof setInterval>;
+// var loading_interval: ReturnType<typeof setInterval>;
 var loading_id = $("#loading-36bec0d286ad9ab9ca8c1d923b0095c1c06");
 
 var ditto = {
@@ -16,12 +16,12 @@ var ditto = {
 
 export function show_loading() {
     loading_id.show();
-    loading_interval = setInterval(function () {
-        loading_id.fadeIn(1000).fadeOut(1000);
-    }, 2000);
+    // loading_interval = setInterval(function () {
+    //     loading_id.fadeIn(1000).fadeOut(1000);
+    // }, 2000);
 }
 export function stop_loading() {
-    clearInterval(loading_interval);
+    // clearInterval(loading_interval);
     loading_id.hide();
     $("#loadingparent-b80f303d89182a8e58b02981c0822deb975").hide();
 }
