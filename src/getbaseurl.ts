628a1e3a39d045dba2584cff22396a7f8f70094a
masx200 <34191203+masx200@users.noreply.github.com>
2021-07-07 17:44:21 +0800

    Update getbaseurl.ts

--- src/getbaseurl.ts
+++ src/getbaseurl.ts
@@ -1,12 +1,13 @@
 //@ts-ignore
 import config from "./config.ts";
-
+//@ts-ignore
+import { urlclearhash } from "./urlclearhash.ts";
 export function getbaseurl() {
     return config.baseurl;
 }
 export function getabsoluteindex() {
-    return String(new URL(config.homepage, config.baseurl));
+    return urlclearhash(String(new URL(config.homepage, config.baseurl)));
 }
 export function getabsolutesummary() {
-    return String(new URL(config.catalogue, config.baseurl));
+    return urlclearhash(String(new URL(config.catalogue, config.baseurl)));
 }
