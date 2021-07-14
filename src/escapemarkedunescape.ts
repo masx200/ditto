df09144753d4b0074431067678d31c0890363ab8
masx200 <34191203+masx200@users.noreply.github.com>
2021-07-14 19:30:32 +0800

    Update escapemarkedunescape.ts

--- src/escapemarkedunescape.ts
+++ src/escapemarkedunescape.ts
@@ -3,11 +3,11 @@ import { escape_html } from "./escape_html.ts";
 //@ts-ignore
 import { unescape_html } from "./unescape_html.ts";
 
-export async function escapemarkedunescape(data: string) {
+export async function escapemarkedunescape(data: string,opts={}) {
     var module = await import("marked");
 
     var marked = module.default;
-    data = marked(escape_html(data));
+    data = marked(escape_html(data),opts);
     data = unescape_html(data);
     return data;
 }
