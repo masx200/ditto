976c1dc3873427195cec89a7b5e61b4897577d36
masx200 <34191203+masx200@users.noreply.github.com>
2021-07-14 18:23:55 +0800

    Update initialize.ts

--- src/initialize.ts
+++ src/initialize.ts
@@ -1,17 +1,26 @@
 //@ts-ignore
 // import hljs from "./highlight.min.ts";
+
+//@ts-ignore
+import { getabsolutesummary } from "./getbaseurl.ts";
 //@ts-ignore
 import { init_sidebar_section } from "./init_sidebar_section.ts";
 //@ts-ignore
+import { resolvemdpathfromhash } from "./resolvemdpathfromhash.ts";
+//@ts-ignore
 import { routerpagegethandler } from "./routerpageget.ts";
-//@ts-ignore
-import { menulist } from "./menulist.ts";
-//@ts-ignore
-import { getabsoluteindex } from "./getbaseurl.ts";
 
 export async function initialize() {
-    // menulist.push(urlclearhash(getabsoluteindex()));
-    await Promise.all([init_sidebar_section(), routerpagegethandler()]); //@ts-ignore
+    const summaryfile = getabsolutesummary();
+    const path = resolvemdpathfromhash();
+    if (Object.is(path, summaryfile)) {
+        await init_sidebar_section();
+        await routerpagegethandler();
+    } else {
+        await Promise.all([init_sidebar_section(), routerpagegethandler()]);
+    }
+
+    //@ts-ignore
     const hljs = (await import("./highlight.min.ts")).default;
     hljs.highlightAll();
 }
