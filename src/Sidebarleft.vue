ba1f5b4bb66c8c52767115d1fd1b38bd4976ed55
masx200 <34191203+masx200@users.noreply.github.com>
2021-07-16 15:02:15 +0800

    优化

--- src/Sidebarleft.vue
+++ src/Sidebarleft.vue
@@ -5,7 +5,7 @@
                 ref="Mysidebar_c41e47b3b3bbc85fdbb7dbba7d3a0743644"
                 id="cebianlan-720feeaf8ee88a6b9b9399876713ed78af4"
                 class="el-container"
-                style="border: 1px solid white; position: fixed"
+                style="border: 1px solid white; position: inherit"
             >
                 <div
                     v-html="html"
