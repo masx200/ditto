f24b42b0a6c47657513d3f917f92242af73e88c9
masx200 <34191203+masx200@users.noreply.github.com>
2021-07-16 23:25:21 +0800

    Update Articlemarkdown.vue

--- src/Articlemarkdown.vue
+++ src/Articlemarkdown.vue
@@ -1,7 +1,7 @@
 
 <template>
     <article>
-        <div
+        <div ref="container"
             id="content-aa867a1d8c5dfbf2abbff8bc2af55a88a36"
             ref="markdowncontent_2e4c728cac441a0c48939881c2c714361a0"
             v-html="html"
