3ea223d39f401077d30e1eb93d2f1e7e06a8c8cb
masx200 <34191203+masx200@users.noreply.github.com>
2021-07-16 23:17:07 +0800

    Update Sidebarleft.vue

--- src/Sidebarleft.vue
+++ src/Sidebarleft.vue
@@ -22,46 +22,9 @@
     </aside>
 </template>
 
-<script>
-//@ts-ignore
-import { debounce } from "./debounce.ts";
-import { defineComponent ,ref,onUnmounted,onMounted} from "@vue/composition-api";
-
-export default defineComponent({
-setup(){
-const height=useinnerhehight()
-return {height}
-},
-    computed: {
-        height() {
-            return (
-                String(this.height - this.top.replaceAll("px", "")) +
-                "px"
-            );
-        },
-    },
-    props: ["html", "top"],
-});
+<script src="./Sidebarleft.ts">
 
 
-
-function useinnerhehight(){
-const height=ref(window.innerHeight)
-const listener=debounce(() => {
-                    requestAnimationFrame(() => {
-                        width.value = window.innerHeight 
-                    });
-                })
-onMounted(()=>{
-window.addEventListener("resize",listener)
-})
-onUnmounted(()=>{
-window.removeEventListener("resize",listener)
-})
-
-
-return height
-}
 </script>
 
 <style scoped>
