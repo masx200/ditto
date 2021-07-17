03a513d99c2c4ce77007a2cb8aa9211ee5171b01
masx200 <34191203+masx200@users.noreply.github.com>
2021-07-17 10:49:34 +0800

    cebianlan-720feeaf8ee88a6b9b9399876713ed78af4

--- src/Sidebarleft.vue
+++ src/Sidebarleft.vue
@@ -3,8 +3,11 @@
         <div class="container-fluid" style="padding: 0">
             <div
                 ref="Mysidebar_c41e47b3b3bbc85fdbb7dbba7d3a0743644"
-                id="cebianlan-720feeaf8ee88a6b9b9399876713ed78af4"
-                class="el-container mysidebarone"
+                class="
+                    el-container
+                    mysidebarone
+                    cebianlan-720feeaf8ee88a6b9b9399876713ed78af4
+                "
                 :style="{
                     'max-height': this.maxheight,
                     top: this.top,
