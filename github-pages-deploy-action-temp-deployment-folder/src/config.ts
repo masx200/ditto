5f1a3213bb61fa074549f600e600b3c6156c76b1
masx200 <34191203+masx200@users.noreply.github.com>
2021-07-18 04:43:47 +0000

    Deploying to gh-pages from @ masx200/markdown-reader@eb4a76dc149fa476c18b53746f940780f338f831 🚀

--- github-pages-deploy-action-temp-deployment-folder/src/config.ts
+++ github-pages-deploy-action-temp-deployment-folder/src/config.ts
@@ -3,5 +3,5 @@ export default {
 
     homepage: "README.md",
     catalogue: "summary.md",
-    baseurl: "https://cdn.jsdelivr.net/gh/masx200/markdown-reader/",
+    baseurl: 'https://masx200.github.io/markdown-reader/'
 };
